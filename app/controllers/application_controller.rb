967c7ed0c27f6501b2a677d8972b8be94044fad2
tomoyama1205 <tomoyama1205@gmail.com>
2019-08-20 22:41:31 +0900

    devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def after_sign_out_path_for(resouce)
+    root_path
+  end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, key: [:nickname])
+  end
 end
