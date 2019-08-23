de1d7968e6dad0de0ccb93fc67a80f957d366095
tomoyama1205 <tomoyama1205@gmail.com>
2019-08-23 16:51:24 +0900

    page-correction

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'shops#index'
   resources :shops, only: :show do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:new, :create] do
       collection do
         get 'search'
       end
+    end
   end
   resources :users, only: :show do
   end
