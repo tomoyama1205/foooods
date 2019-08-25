99c35fc566170fdc05c031873b57c99bd050c5c2
tomoyama1205 <tomoyama1205@gmail.com>
2019-08-25 12:04:19 +0900

    ranking

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :ranking, only: new
 
   def new
     @shop = Shop.find(params[:shop_id])
@@ -11,7 +12,13 @@ class ReviewsController < ApplicationController
   end
 
   private
+
   def create_params
     params.require(:review).permit(:rate, :review).merge(shop_id: params[:shop_id], user_id: current_user.id)
+  end
+
+  def ranking
+    shop_ids = @shops.reviews.average(:rate).order('count_shop_id DESC').limit(3).count(:shop_id).keys
+    @ranking = shop_ids.map { |id| Shop.find(id) }
   end
 end
--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,6 @@
 class ShopsController < ApplicationController
   before_action :authenticate_user!, only: :search
+  before_action :ranking
 
   def index
     @shops = Shop.all
@@ -12,5 +13,11 @@ class ShopsController < ApplicationController
   def search
     @shops = Shop.where('title LIKE(?)', "%#{params[:keyword]}%")
   end
-  
+
+  private
+
+  def ranking
+    shop_ids = Review.group(:shop_id).order('average_rate DESC').limit(3).average(:rate).keys
+    @ranking = shop_ids.map { |id| Shop.find(id) }
+  end
 end
