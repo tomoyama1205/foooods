b12b478a44b1ac164faa99622f33de0108863cfb
tomoyama1205 <tomoyama1205@gmail.com>
2019-08-25 15:14:59 +0900

    mypage

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20190821131413) do
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
